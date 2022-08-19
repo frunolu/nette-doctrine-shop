df2935c1e97e6926635662fa1360b6fcb881a6e6
frunolu <fruno.lu@gmail.com>
2022-08-19 10:59:01 +0200

    Add HomepagePresenter, Modify @layout.latte

--- src/app/Presenters/HomepagePresenter.php
+++ src/app/Presenters/HomepagePresenter.php
@@ -4,9 +4,19 @@ declare(strict_types=1);
 
 namespace App\Presenters;
 
+use App\Model\Repository\CategoryRepository;
 use Nette;
-
 
 final class HomepagePresenter extends Nette\Application\UI\Presenter
 {
+
+    /**
+     * @var CategoryRepository @inject
+     */
+    public CategoryRepository $categoryRepository;
+
+    public function renderDefault():void
+    {
+        $this->template->categories = $this->categoryRepository->findAll();
+    }
 }
