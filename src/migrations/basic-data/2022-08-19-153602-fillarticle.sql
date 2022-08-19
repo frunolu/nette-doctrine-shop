e2267e18eafcbcc8a6c6c389f129d57f8ca428b0
frunolu <fruno.lu@gmail.com>
2022-08-19 15:44:34 +0200

    Add fillarticle.sql

---
+++ src/migrations/basic-data/2022-08-19-153602-fillarticle.sql
@@ -0,0 +1,7 @@
+
+INSERT INTO `article` (`title`, `content`, `released`, `released_date`)
+VALUES ('title 1', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor '
+, 1, now());
+INSERT INTO `article` (`title`, `content`, `released`, `released_date`)
+VALUES ('title 2', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor '
+, 1, now());
