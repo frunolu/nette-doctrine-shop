00f984834020109855b85751177109f3f08b6105
frunolu <fruno.lu@gmail.com>
2022-08-19 15:10:19 +0200

    Add entity Role, RoleRepository, fillrole.sql

---
+++ src/app/Model/User/Role.php
@@ -0,0 +1,63 @@
+<?php
+
+
+namespace App\Model\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\ORM\Mapping as ORM;
+
+
+/**
+ * @package App\Model\Entity
+ * @ORM\Entity
+ */
+class Role
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue
+     */
+    public $id;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    public $name;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name): void
+    {
+        $this->name = $name;
+    }
+
+
+}
+
+
---
+++ src/migrations/basic-data/2022-08-19-150631-fillrole.sql
@@ -0,0 +1,4 @@
+DELETE FROM `role`;
+INSERT INTO `role` (`name`) VALUES ('guest');
+INSERT INTO `role` (`name`) VALUES ('admin');
+INSERT INTO `role` (`name`) VALUES ('moderator');
