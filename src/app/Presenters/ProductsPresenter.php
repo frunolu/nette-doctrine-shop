541d093b24b04afd464cead4cbacd4b9bd0a9c03
frunolu <fruno.lu@gmail.com>
2022-08-19 11:54:43 +0200

    Add ProductsPresenter, template Products:default.latte, route

---
+++ src/app/Presenters/ProductsPresenter.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Presenters;
+
+use App\Model\Repository\ProductRepository;
+use Tracy\Debugger;
+use Nette\Utils\Paginator;
+use Nette;
+
+class ProductsPresenter extends Nette\Application\UI\Presenter
+{
+    /**
+     * @var ProductRepository @inject
+     */
+    public ProductRepository $productRepository;
+
+    /**
+     * @var int $ItemsPerPage
+     */
+    private $itemsPerPage = 10;
+
+    public function renderDefault(int $page = 1): void
+    {
+        $productsCount = count($this->productRepository->findAll());
+        $this->template->productsCount = $productsCount;
+
+        $paginator = new Paginator;
+        $paginator->setItemCount($productsCount);
+        $paginator->setItemsPerPage($this->itemsPerPage);
+        $paginator->setPage($page);
+
+        $products = $this->productRepository->findBy
+        (
+            [],
+            null,
+            $paginator->getLength(),
+            $paginator->getOffset()
+        );
+
+        $this->template->products = $products;
+        $this->template->paginator = $paginator;
+    }
+
+
+}
--- src/app/Router/RouterFactory.php
+++ src/app/Router/RouterFactory.php
@@ -11,12 +11,13 @@ use Nette\StaticClass;
 
 final class RouterFactory
 {
-	use StaticClass;
+    use StaticClass;
 
-	public static function createRouter(): RouteList
-	{
-		$router = new RouteList;
-		$router->addRoute('<presenter>/<action>[/<id>]', 'Homepage:default');
-		return $router;
-	}
+    public static function createRouter(): RouteList
+    {
+        $router = new RouteList;
+        $router->addRoute('Produkty/', 'Products:default');
+        $router->addRoute('<presenter>/<action>[/<id>]', 'Homepage:default');
+        return $router;
+    }
 }
